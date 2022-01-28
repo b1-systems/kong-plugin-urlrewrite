4a3c252d1f82ceb5699143f893e4cc03095f717c
Felix Glaser <glaser@b1-systems.de>
2022-01-28 10:12:23 +0100

    add field for the rewrite header in the schema

--- kong/plugins/urlrewrite/schema.lua
+++ kong/plugins/urlrewrite/schema.lua
@@ -6,7 +6,6 @@ local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
 local schema = {
   name = plugin_name,
   fields = {
-    -- the 'fields' array is the top-level entry with fields defined by Kong
     -- this plugin cannot be configured on a consumer
     { consumer = typedefs.no_consumer },
     -- this plugin cannot be configured on a service
@@ -15,28 +14,32 @@ local schema = {
     -- TODO: should also work on https
     { protocols = typedefs.protocols_http },
     { config = {
-        -- The 'config' record is the custom part of the plugin schema
         type = "record",
         fields = {
+            { rewrite_header = typedefs.header_name {
+                    required = true,
+                    default = "Rewrite-To",
+                }
+            }
           -- a standard defined field (typedef), with some customizations
-          { request_header = typedefs.header_name {
-              required = true,
-              default = "Hello-World" } },
-          { response_header = typedefs.header_name {
-              required = true,
-              default = "Bye-World" } },
-          { ttl = { -- self defined field
-              type = "integer",
-              default = 600,
-              required = true,
-              gt = 0, }}, -- adding a constraint for the value
+          --{ request_header = typedefs.header_name {
+          --    required = true,
+          --    default = "Hello-World" } },
+          --{ response_header = typedefs.header_name {
+          --    required = true,
+          --    default = "Bye-World" } },
+          --{ ttl = { -- self defined field
+          --    type = "integer",
+          --    default = 600,
+          --    required = true,
+          --    gt = 0, }}, -- adding a constraint for the value
         },
         entity_checks = {
           -- add some validation rules across fields
           -- the following is silly because it is always true, since they are both required
-          { at_least_one_of = { "request_header", "response_header" }, },
+          --{ at_least_one_of = { "request_header", "response_header" }, },
           -- We specify that both header-names cannot be the same
-          { distinct = { "request_header", "response_header"} },
+          --{ distinct = { "request_header", "response_header"} },
         },
       },
     },
