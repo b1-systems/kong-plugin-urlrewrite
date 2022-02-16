aca539a4017e0b89449d97683c1ffdfecc6dcea8
Felix Glaser <glaser@b1-systems.de>
2022-02-16 14:44:33 +0100

    change scheme, host and path based on URL given in header

--- kong/plugins/urlrewrite/handler.lua
+++ kong/plugins/urlrewrite/handler.lua
@@ -64,13 +64,13 @@ end --]]
 
 -- runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
-  -- check if header exists
-  -- check if header is not empty
   local rewrite_header = kong.request.get_header(plugin_conf.rewrite_header)
   if rewrite_header == "" then
+    -- check if header is not empty
     kong.log.err("Header is empty")
     return kong.response.exit(400, "Bad request")
   elseif rewrite_header == nil then
+    -- check if header exists
     kong.log.err("Header is nil")
     return kong.response.exit(400, "Bad request")
   end
@@ -83,6 +83,24 @@ function plugin:access(plugin_conf)
   end
 
   kong.log.inspect({scheme=scheme, host=host, path=path})
+
+  if scheme ~= nil and scheme ~= "" then
+    if kong.request.get_scheme() ~= scheme then
+      kong.service.request.set_scheme(scheme)
+    end
+  end
+
+  if host ~= nil and host ~= "" then
+    if kong.request.get_host() ~= host then
+      kong.service.request.set_header("Host", host)
+    end
+  end
+
+  if path ~= nil and path ~= "" then
+    if kong.request.get_path() ~= path then
+      kong.service.request.set_path(path)
+    end
+  end
 
 end --]]
 
