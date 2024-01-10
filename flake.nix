0fbb0733c4689387c8c0fe809c2ae5ffcd958d04
Felix Glaser <glaser@b1-systems.de>
2024-01-10 11:40:23 +0100

    update pongo version

--- flake.nix
+++ flake.nix
@@ -2,60 +2,54 @@
   description = "Kong plugin development environment";
 
   inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
 
-    pongo = {
-      type = "github";
-      owner = "Kong";
-      repo = "kong-pongo";
-      ref = "2.6.0";
-      flake = false;
-    };
+    ikselven-packages.url = "git+https://codeberg.org/ikselven/nix-packages?ref=main";
+    ikselven-packages.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, utils, pongo }:
-    utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = utils.lib.flattenTree {
-          deck = pkgs.buildGoModule rec {
-            pname = "deck";
-            version = "1.19.1";
+  outputs = {
+    nixpkgs,
+    utils,
+    ikselven-packages,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs-ikselven = ikselven-packages.packages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
 
-            src = pkgs.fetchFromGitHub {
-              owner = "Kong";
-              repo = pname;
-              rev = "v${version}";
-              sha256 = "sha256-ao3SGtHwDH+1zTYAzeN37cRNLjCaSFULkXOxLTKTvxc=";
-            };
+        overlays = [
+          (final: prev: {
+            deck = pkgs-ikselven.deck;
+            kong-pongo = pkgs-ikselven.kong-pongo;
+          })
+        ];
+      };
+    in rec {
+      formatter = pkgs.alejandra;
 
-            vendorSha256 =
-              "sha256-xFynhV1z3+8K613pFxoVxxQeG1N+rV8/gzkpy9MPF/0=";
-
-            excludedPackages = [ ];
-
-            ldflags = [ "-s" "-w" "-X main.version=v${version}" ];
-          };
-
-          pongo = pkgs.writeShellApplication {
-            name = "pongo";
-            text = ''
-              ${pongo.outPath}/pongo.sh "$@"
-            '';
-          };
-        };
-
-        # defines a development environment with pongo available
-        # preferably use direnv to activate
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [ cassowary curl docker-compose fx jq pgcli ]
-            ++ [ packages.deck packages.pongo ] ++ (with pkgs.luaPackages; [
-              luarocks
-              luacov
-              luacheck
-              luaformatter
-            ]);
-        };
-      });
+      # defines a development environment with pongo available
+      # preferably use direnv to activate
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [
+            cassowary
+            curl
+            deck
+            docker-compose
+            fx
+            jq
+            kong-pongo
+            pgcli
+          ]
+          ++ (with pkgs.luaPackages; [
+            luarocks
+            luacov
+            luacheck
+            luaformatter
+          ]);
+      };
+    });
 }
